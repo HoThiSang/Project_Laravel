092f4d000d1e68021255ab3009876fb984ad45c1
HoMaiHuyen <huyen.homai25@student.passerellesnumeriques.org>
2024-04-14 17:43:34 +0700

    admin 's order update layout

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -1,10 +1,14 @@
 <?php
 
+
 namespace App\Models;
+
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class Order extends Model
 {
@@ -15,4 +19,19 @@ class Order extends Model
     {
         return DB::table($this->table)->insertGetId($data);
     }
+  
+
+
+    use SoftDeletes;
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+
+    protected $fillable = [
+        'username', 'address', 'phone_number', 'payment_method'
+    ];
 }
